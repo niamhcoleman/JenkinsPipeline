9e6bc4a504a54fa3c714afe14d62a9d08cadceee
niamhcoleman <35526308+niamhcoleman@users.noreply.github.com>
2019-01-04 19:04:58 +0000

    Update StudentTest.java

--- StudentTest.java
+++ StudentTest.java
@@ -1,12 +1,11 @@
-//c00205225
-//Niamh Coleman
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by meudecc on 15/02/2017.
  */
-public class StudentTest {
+public class studentTest {
     @org.junit.Test(expected = IllegalArgumentException.class)
     public void belowZero() throws Exception {
         Student std = new Student();
