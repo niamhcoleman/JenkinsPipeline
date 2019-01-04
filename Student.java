c3fb47d50102f5dc2192710c8a3a6dbf840e301a
niamhcoleman <35526308+niamhcoleman@users.noreply.github.com>
2019-01-04 20:14:04 +0000

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,6 +1,3 @@
-/**
- * Created by meudecc on 15/02/2017.
- */
 public class Student {
 
     public enum AttendanceGrade {ABSENT, VERY_POOR, POOR, AVERAGE, GOOD, VERY_GOOD};
@@ -8,9 +5,9 @@ public class Student {
     public AttendanceGrade getAttendanceGrade(int attendance) {
         if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
         if (attendance == 0) return AttendanceGrade.ABSENT;
-        if ((attendance > 0) && (attendance < 30)) return AttendanceGrade.VERY_POOR;
-        if ((attendance >= 30) && (attendance < 70)) return AttendanceGrade.AVERAGE;
-        if ((attendance >= 70) && (attendance < 90)) return AttendanceGrade.GOOD;
-        if ((attendance >= 90) && (attendance < 100)) return AttendanceGrade.VERY_GOOD;
+        if (attendance < 30) return AttendanceGrade.VERY_POOR;
+        if (attendance < 70) return AttendanceGrade.AVERAGE;
+        if (attendance < 90) return AttendanceGrade.GOOD;
+        return AttendanceGrade.VERY_GOOD;
     }
 }
